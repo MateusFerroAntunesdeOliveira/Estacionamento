fa788965ae06e652fde6ca4a804dd0be37f1a68b
Mateus <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2021-05-05 18:13:10 -0300

    Cadastrar entrada quase pronta - cadastrar marca feito - cadastrar modelo quase pronto

---
+++ Estacionamento/src/Modelagem/Carro.java
@@ -0,0 +1,116 @@
+package Modelagem;
+
+import java.time.LocalDateTime;
+
+public class Carro {
+
+	// -> Inicialização
+	private String placa;
+	private float valor;
+	private Modelo modelo;
+	private LocalDateTime horaEntrada;
+	private LocalDateTime horaSaida;
+
+	// -> Construtores
+	public Carro() {
+		super();
+		placa = "Sem placa";
+		valor = 0;
+		modelo = new Modelo();
+		horaEntrada = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+		horaSaida = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+	}
+
+	public Carro(String placa) {
+		super();
+		this.placa = placa;
+		valor = 0;
+		modelo = new Modelo();
+		horaEntrada = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+		horaSaida = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+	}
+
+	public Carro(String placa, float valor) {
+		super();
+		this.placa = placa;
+		this.valor = valor;
+		modelo = new Modelo();
+		horaEntrada = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+		horaSaida = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+	}
+
+	public Carro(String placa, Modelo modelo) {
+		super();
+		this.placa = placa;
+		valor = 0;
+		this.modelo = modelo;
+		horaEntrada = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+		horaSaida = LocalDateTime.of(1900, 1, 1, 12, 0, 0);
+	}
+
+	public Carro(String placa, float valor, Modelo modelo) {
+		super();
+		this.placa = placa;
+		this.valor = valor;
+		this.modelo = modelo;
+		horaEntrada = LocalDateTime.now();
+	}
+
+	public Carro(String placa, float valor, Modelo modelo, LocalDateTime horaEntrada) {
+		super();
+		this.placa = placa;
+		this.valor = valor;
+		this.modelo = modelo;
+		this.horaEntrada = LocalDateTime.now();
+	}
+
+	public Carro(String placa, Modelo modelo, LocalDateTime horaSaida) {
+		super();
+		this.placa = placa;
+		valor = 0;
+		this.modelo = modelo;
+		this.horaSaida = LocalDateTime.now();
+	}
+
+	// -> Getters e Setters
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public LocalDateTime getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(LocalDateTime horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public LocalDateTime getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(LocalDateTime horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+}
---
+++ Estacionamento/src/Modelagem/Modelo.java
@@ -0,0 +1,25 @@
+package Modelagem;
+
+public class Modelo {
+
+	// -> Inicialização
+	private String nome;
+
+	// -> Construtores
+	public Modelo() {
+		nome = "Sem modelo";
+	}
+
+	public Modelo(String nome) {
+		this.nome = nome;
+	}
+
+	// -> Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
